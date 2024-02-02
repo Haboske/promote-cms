15b7f5a78fd0b1f48058cd9a768d2928bde6c41b
Haboske <74014416+Haboske@users.noreply.github.com>
2024-02-02 16:01:42 +0100

    Homepage development

--- index.php
+++ index.php
@@ -29,6 +29,110 @@
                 <img src="content/PNG/homepage-target.png" class="homepage-target">
             </div>
         </section>
+        <section class="section-spacing background-color-black">
+            <div class="wrapper display-flex small-gap ">
+                <div class="wrapper">
+                    <div class="icon blue">
+                    </div>
+                </div>
+                <div class="wrapper display-flex flex-row flew-nowrap flex-between huge-gap">
+                    <div class="wrapper display-flex flex-column">
+                        <p class="text-uppercase text-size-small">
+                            <span class="icon-slash text-color-yellow">// </span>01 . Services
+                        </p>  
+                        <h2 class="heading-medium">Des services à fort impact pour faire passer votre entreprise au niveau supérieur</h2>
+                    </div>
+                    <div class="wrapper display-flex justify-end align-end">
+                        <a class="button small white">Nos services ></a>
+                    </div>
+                </div>
+            </div>
+            <div class="wrapper display-grid-3col-2row margin-top-medium">
+                <div class="card grid-child-1 background-gradient-softBlack padding-small display-flex flex-column small-gap">
+                    <div class="icon huge black">
+                        <img class="image" src="content/PNG/services-chat-marketing.png">
+                    </div>
+                    <h3 class="heading-tiny-small">Marketing réseaux sociaux</h3>
+                    <p>Lorem ipsum dolor sit amet, ea mundi malorum cum, ocurreret reprehendunt. </p>
+                    <a class="button transparent margin-top-auto">Voir Plus ></a>
+                </div>
+                
+                <div class="card grid-child-2 background-gradient-softBlack padding-small display-flex flex-column small-gap">
+                    <div class="icon huge black">
+                        <img class="image" src="content/PNG/services-megaphone.png">
+                    </div>
+                    <h3 class="heading-tiny-small">Publicité payée</h3>
+                    <p>Lorem ipsum dolor sit amet, ea mundi malorum cum, ocurreret reprehendunt. </p>
+                    <a class="button transparent margin-top-auto">Voir Plus ></a>
+                </div>
+
+                <div class="card grid-child-3 background-gradient-softBlack padding-small display-flex flex-column small-gap">
+                    <div class="icon huge black">
+                        <img class="image" src="content/PNG/services-statistiques.png">
+                    </div>
+                    <h3 class="heading-tiny-small">Analyses avancées</h3>
+                    <p>Lorem ipsum dolor sit amet, ea mundi malorum cum, ocurreret reprehendunt. </p>
+                    <a class="button transparent margin-top-auto">Voir Plus ></a>
+                </div>
+
+                <div class="card grid-child-4 background-gradient-softBlack padding-small display-flex flex-column small-gap">
+                    <div class="icon huge black">
+                        <img class="image" src="content/PNG/services-startup.png">
+                    </div>
+                    <h3 class="heading-tiny-small">SEO Optimisation</h3>
+                    <p>Lorem ipsum dolor sit amet, ea mundi malorum cum, ocurreret reprehendunt. </p>
+                    <a class="button transparent margin-top-auto">Voir Plus ></a>
+                </div>
+
+                <div class="card grid-child-5 background-gradient-softBlack padding-small display-flex flex-column small-gap">
+                    <div class="icon huge black">
+                        <img class="image" src="content/PNG/services-medal.png">
+                    </div>
+                    <h3 class="heading-tiny-small">Tunnel Optimisation</h3>
+                    <p>Lorem ipsum dolor sit amet, ea mundi malorum cum, ocurreret reprehendunt. </p>
+                    <a class="button transparent margin-top-auto">Voir Plus ></a>
+                </div>
+
+                <div class="card grid-child-6 background-gradient-softBlack padding-small display-flex flex-column small-gap">
+                    <div class="icon huge black">
+                        <img class="image" src="content/PNG/services-checklist.png">
+                    </div>
+                    <h3 class="heading-tiny-small">Marketing de contenu</h3>
+                    <p>Lorem ipsum dolor sit amet, ea mundi malorum cum, ocurreret reprehendunt. </p>
+                    <a class="button transparent margin-top-auto">Voir Plus ></a>
+                </div>            
+            </div>
+        </section>
+        <section class="section-spacing display-grid-2col-1row">
+            <div class="wrapper grid-child-1">
+
+            </div>
+            <div class="wrapper grid-child-2 display-flex flex-column">
+                <div class="wrapper display-flex small-gap ">
+                    <div class="wrapper">
+                        <div class="icon yellow">
+                        </div>
+                    </div>
+                    <div class="wrapper display-flex flex-row flew-nowrap flex-between huge-gap">
+                        <div class="wrapper display-flex flex-column">
+                            <p class="text-uppercase text-size-small">
+                                <span class="icon-slash text-color-yellow">// </span>02 . A propos
+                            </p>  
+                            <h2 class="heading-medium">L’agence marketing #1 dans le digital</h2>
+                        </div>
+                    </div>
+                </div>
+                <div class="divider margin-top-small"></div>
+                <p class="margin-top-small">Lorem Ipsum is simply dummy text of the printing and typesetting industry ftyuu Ipsum has been the industry's standard book. It has survived not only five centuries, but also the leap into electronic typesetting.</p>
+                <div class="wrapper margin-left-medium margin-top-small">
+                    <div class="icon white">
+
+                    </div>
+                </div>
+            </div>
+        </section>
+
+
     </main>
     <
     <?php require 'api/footer.php' ?>
